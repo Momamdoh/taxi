0a04b203b0cdb71c394b0911ebe76689ada12057
Momamdoh <momamdoh4231@gmail.com>
2025-07-15 11:10:02 +0300

    Remove likes.js from routes

--- routes/likes.js
+++
@@ -1,9 +0,0 @@
-// routes/likes.js
-const express = require("express");
-const router = express.Router();
-const { likeOrDislikeDriver, getDriverLikes } = require("../controllers/likeController");
-
-router.post("/", likeOrDislikeDriver);
-router.get("/driver/:driverId", getDriverLikes);
-
-module.exports = router;
